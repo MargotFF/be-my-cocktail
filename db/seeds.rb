ea1257a50509fcedd96d8e38e71368d3b013f800
Margot Fessard <fessard.margot@gmail.com>
2020-10-24 14:40:29 +0200

    Seed, Routes, Controller, Views for Cocktails (no front)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying cocktails...'
+Cocktail.destroy_all
+puts 'Cocktails destroyed!'
+
+puts 'Creating cocktails...'
+
+mojito = Cocktail.create(name: 'Mojito')
+pisco_sour = Cocktail.create(name: 'Pisco Sour')
+bloody_mary = Cocktail.create(name: 'Bloody Mary')
+
+puts 'Cocktails created!'
+
+puts 'Destroying ingredients...'
+Ingredient.destroy_all
+puts 'Ingredients destroyed!'
+
+puts 'Creating ingredients...'
+
+rum = Ingredient.create(name: 'rum')
+soda_water = Ingredient.create(name: 'soda water')
+lemon = Ingredient.create(name: 'lemon')
+ice = Ingredient.create(name: 'ice')
+mint_leaves = Ingredient.create(name: 'mint leaves')
+sugar = Ingredient.create(name: 'sugar')
+pisco = Ingredient.create(name: 'pisco')
+egg_white = Ingredient.create(name: 'egg white')
+vodka = Ingredient.create(name: 'vodka')
+tomato_juice = Ingredient.create(name: 'tomato juice')
+tabasco = Ingredient.create(name: 'tabasco')
+gin = Ingredient.create(name: 'gin')
+tonic = Ingredient.create(name: 'tonic')
+
+puts 'Ingredients created!'
+
+puts 'Destroying doses...'
+Dose.destroy_all
+puts 'Doses destroyed!'
+
+puts 'Creating doses...'
+
+Dose.create(description: '5 cubes', cocktail: mojito, ingredient: ice)
+
+puts 'Doses created!'
