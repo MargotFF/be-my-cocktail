7e0e25d948690835edf4da98af040e4b73d29b0f
Margot Fessard <fessard.margot@gmail.com>
2020-10-30 12:07:46 +0100

    Seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,12 @@ puts 'Doses destroyed!'
 
 puts 'Creating cocktails...'
 
-mojito = Cocktail.create(name: 'Mojito', description: 'The Mojito might be Cuba’s most popular contribution to cocktail culture. The simple mix of white rum, lime juice, cane sugar, and soda (with muddled mint, please) is fresh and tropical, and it’s a classic.')
-pisco_sour = Cocktail.create(name: 'Pisco Sour', description: 'The Pisco Sour, made with the Peruvian and Chilean national spirit pisco, along with lime, sugar, and an optional egg white, is appropriate for any occasion.')
-bloody_mary = Cocktail.create(name: 'Bloody Mary', description: 'The Bloody Mary is as much an experience as a drink. The brunch-time staple is best enjoyed with a house mix of tomato juice, vodka, and spices.')
-gin_tonic = Cocktail.create(name: 'Gin Tonic', description: "Classic and easy, the gin and tonic (or G&T) is light and refreshing. It's a simple mixed drink that requires just the two named ingredients and a hint of lime, all of which are natural flavor companions.")
+mojito = Cocktail.create(name: 'Mojito', description: 'The Mojito might be Cuba’s most popular contribution to cocktail culture. The mojito is the epitome of the refreshing cocktail, stripped down to just the bare essentials of rum, lime juice, sugar, soda water, and mint. Each ingredient is seemingly specifically selected to cure hot weather-induced pangs of thirst.')
+pisco_sour = Cocktail.create(name: 'Pisco Sour', description: "Peru’s and Chile's signature drink, the famous Pisco Sour has a taste quite unlike any other. It’s a curious mix of sharp tang and sweet, and its texture changes from cloudlike foam to icy cold in the time it takes for the liquid to break through its wall of raw egg white.")
+bloody_mary = Cocktail.create(name: 'Bloody Mary', description: 'The Bloody Mary is a vodka-soaked nutritional breakfast and hangover cure all in one. What else could you ask for? This brunch-time staple is best enjoyed with a house mix of tomato juice, vodka, and spices. It is as much an experience as a drink.')
+gin_tonic = Cocktail.create(name: 'Gin Tonic', description: "Classic and easy, the gin and tonic (or G&T) is light and refreshing. It's a simple mixed drink that requires just the two named ingredients and a hint of lime, all of which are natural flavor companions. This is a great choice for happy hour, dinner, or anytime you simply want an invigorating beverage.")
+pina_colada = Cocktail.create(name: 'Piña Colada', description: 'The Piña Colada is a sweet, creamy, frozen drink bursting with fruity flavors from pineapple and coconut.  The flavors are so vibrant that they will transport you from your porch to a lounge chair at a tropical beach!')
+margarita = Cocktail.create(name: 'Margarita', description: "There is no mystery to this iconic drink, and the secret to a great margarita is its simplicity. The only ingredients you need are tequila, triple sec, and lime juice. Once you find an ideal balance of flavor, you'll realize just how delicious a fresh margarita is.")
 
 puts 'Cocktails created!'
 
@@ -41,11 +43,47 @@ tomato_juice = Ingredient.create(name: 'tomato juice')
 tabasco = Ingredient.create(name: 'tabasco')
 gin = Ingredient.create(name: 'gin')
 tonic = Ingredient.create(name: 'tonic')
+pineapple_juice = Ingredient.create(name: 'pineapple juice')
+coconut_cream = Ingredient.create(name: 'coconut cream')
+tequila = Ingredient.create(name: 'tequila')
+triple_sec = Ingredient.create(name: 'triple sec')
 
 puts 'Ingredients created!'
 
 puts 'Creating doses...'
 
-Dose.create(description: '5 cubes', cocktail: mojito, ingredient: ice)
+Dose.create(description: 'large handful', cocktail: mojito, ingredient: ice)
+Dose.create(description: '1/2', cocktail: mojito, ingredient: lime)
+Dose.create(description: '20 ml', cocktail: mojito, ingredient: sugar_syrup)
+Dose.create(description: '6', cocktail: mojito, ingredient: mint_leaves)
+Dose.create(description: '60 ml', cocktail: mojito, ingredient: rum)
+Dose.create(description: 'to top', cocktail: mojito, ingredient: soda_water)
+
+Dose.create(description: 'large handful', cocktail: pisco_sour, ingredient: ice)
+Dose.create(description: '30 ml', cocktail: pisco_sour, ingredient: lime)
+Dose.create(description: '20 ml', cocktail: pisco_sour, ingredient: sugar_syrup)
+Dose.create(description: '1', cocktail: pisco_sour, ingredient: egg_white)
+Dose.create(description: '50 ml', cocktail: pisco_sour, ingredient: pisco)
+
+Dose.create(description: 'large handful', cocktail: bloody_mary, ingredient: ice)
+Dose.create(description: '1/2', cocktail: bloody_mary, ingredient: lime)
+Dose.create(description: '3 dashes', cocktail: bloody_mary, ingredient: tabasco)
+Dose.create(description: '40 ml', cocktail: bloody_mary, ingredient: vodka)
+Dose.create(description: '120 ml', cocktail: bloody_mary, ingredient: tomato_juice)
+
+Dose.create(description: 'large handful', cocktail: gin_tonic, ingredient: ice)
+Dose.create(description: 'a wedge', cocktail: gin_tonic, ingredient: lime)
+Dose.create(description: '50 ml', cocktail: gin_tonic, ingredient: gin)
+Dose.create(description: '150 ml', cocktail: gin_tonic, ingredient: tonic)
+
+Dose.create(description: 'large handful', cocktail: pina_colada, ingredient: ice)
+Dose.create(description: '60 ml', cocktail: pina_colada, ingredient: rum)
+Dose.create(description: '120 ml', cocktail: pina_colada, ingredient: pineapple_juice)
+Dose.create(description: '60 ml', cocktail: pina_colada, ingredient: coconut_cream)
+
+Dose.create(description: 'large handful', cocktail: margarita, ingredient: ice)
+Dose.create(description: '25 ml', cocktail: margarita, ingredient: lime)
+Dose.create(description: '50 ml', cocktail: margarita, ingredient: tequila)
+Dose.create(description: '20 ml', cocktail: margarita, ingredient: triple_sec)
 
 puts 'Doses created!'
