5c2cf2dbfa1ad0dc18307767b36329c2e7d0e0d4
Margot Fessard <fessard.margot@gmail.com>
2020-11-14 20:34:44 +0100

    Cloudinary: photos cocktails (seed),  upload picture (create/edit) + previous/next buttons on show

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,16 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
+
+  def previous
+    Cocktail.where('id < ?', id).first
+  end
+
+  def next
+    Cocktail.where('id > ?', id).first
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,35 @@ puts 'Doses destroyed!'
 
 puts 'Creating cocktails...'
 
-mojito = Cocktail.create(name: 'Mojito', description: 'The Mojito might be Cuba’s most popular contribution to cocktail culture. The mojito is the epitome of the refreshing cocktail, stripped down to just the bare essentials of rum, lime juice, sugar, soda water, and mint. Each ingredient is seemingly specifically selected to cure hot weather-induced pangs of thirst.')
-pisco_sour = Cocktail.create(name: 'Pisco Sour', description: "Peru’s and Chile's signature drink, the famous Pisco Sour has a taste quite unlike any other. It’s a curious mix of sharp tang and sweet, and its texture changes from cloudlike foam to icy cold in the time it takes for the liquid to break through its wall of raw egg white.")
-bloody_mary = Cocktail.create(name: 'Bloody Mary', description: 'The Bloody Mary is a vodka-soaked nutritional breakfast and hangover cure all in one. What else could you ask for? This brunch-time staple is best enjoyed with a house mix of tomato juice, vodka, and spices. It is as much an experience as a drink.')
-gin_tonic = Cocktail.create(name: 'Gin Tonic', description: "Classic and easy, the gin and tonic (or G&T) is light and refreshing. It's a simple mixed drink that requires just the two named ingredients and a hint of lime, all of which are natural flavor companions. This is a great choice for happy hour, dinner, or anytime you simply want an invigorating beverage.")
-pina_colada = Cocktail.create(name: 'Piña Colada', description: 'The Piña Colada is a sweet, creamy, frozen drink bursting with fruity flavors from pineapple and coconut.  The flavors are so vibrant that they will transport you from your porch to a lounge chair at a tropical beach!')
-margarita = Cocktail.create(name: 'Margarita', description: "There is no mystery to this iconic drink, and the secret to a great margarita is its simplicity. The only ingredients you need are tequila, triple sec, and lime juice. Once you find an ideal balance of flavor, you'll realize just how delicious a fresh margarita is.")
+mojito = Cocktail.new(name: 'Mojito', description: 'The Mojito might be Cuba’s most popular contribution to cocktail culture. The mojito is the epitome of the refreshing cocktail, stripped down to just the bare essentials of rum, lime juice, sugar, soda water, and mint. Each ingredient is seemingly specifically selected to cure hot weather-induced pangs of thirst.')
+file = URI.open('https://res.cloudinary.com/dl1tipgzw/image/upload/v1605290630/Cocktails/mojito2.jpg')
+mojito.photo.attach(io: file, filename: 'mojito.jpg', content_type: 'image/jpg')
+mojito.save!
+
+pisco_sour = Cocktail.new(name: 'Pisco Sour', description: "Peru’s and Chile's signature drink, the famous Pisco Sour has a taste quite unlike any other. It’s a curious mix of sharp tang and sweet, and its texture changes from cloudlike foam to icy cold in the time it takes for the liquid to break through its wall of raw egg white.")
+file = URI.open('https://res.cloudinary.com/dl1tipgzw/image/upload/v1605281857/Cocktails/pisco_sour.jpg')
+pisco_sour.photo.attach(io: file, filename: 'pisco_sour.jpg', content_type: 'image/jpg')
+pisco_sour.save!
+
+bloody_mary = Cocktail.new(name: 'Bloody Mary', description: 'The Bloody Mary is a vodka-soaked nutritional breakfast and hangover cure all in one. What else could you ask for? This brunch-time staple is best enjoyed with a house mix of tomato juice, vodka, and spices. It is as much an experience as a drink.')
+file = URI.open('https://res.cloudinary.com/dl1tipgzw/image/upload/v1605281858/Cocktails/bloody_mary.jpg')
+bloody_mary.photo.attach(io: file, filename: 'bloody_mary.jpg', content_type: 'image/jpg')
+bloody_mary.save!
+
+gin_tonic = Cocktail.new(name: 'Gin Tonic', description: "Classic and easy, the gin and tonic (or G&T) is light and refreshing. It's a simple mixed drink that requires just the two named ingredients and a hint of lime, all of which are natural flavor companions. This is a great choice for happy hour, dinner, or anytime you simply want an invigorating beverage.")
+file = URI.open('https://res.cloudinary.com/dl1tipgzw/image/upload/v1605281857/Cocktails/gin_tonic.jpg')
+gin_tonic.photo.attach(io: file, filename: 'gin_tonic.jpg', content_type: 'image/jpg')
+gin_tonic.save!
+
+pina_colada = Cocktail.new(name: 'Piña Colada', description: 'The Piña Colada is a sweet, creamy, frozen drink bursting with fruity flavors from pineapple and coconut.  The flavors are so vibrant that they will transport you from your porch to a lounge chair at a tropical beach!')
+file = URI.open('https://res.cloudinary.com/dl1tipgzw/image/upload/v1605289932/Cocktails/pina_colada2.jpg')
+pina_colada.photo.attach(io: file, filename: 'pina_colada.jpg', content_type: 'image/jpg')
+pina_colada.save!
+
+margarita = Cocktail.new(name: 'Margarita', description: "There is no mystery to this iconic drink, and the secret to a great margarita is its simplicity. The only ingredients you need are tequila, triple sec, and lime juice. Once you find an ideal balance of flavor, you'll realize just how delicious a fresh margarita is.")
+file = URI.open('https://res.cloudinary.com/dl1tipgzw/image/upload/v1605281858/Cocktails/margarita.jpg')
+margarita.photo.attach(io: file, filename: 'margarita.jpg', content_type: 'image/jpg')
+margarita.save!
 
 puts 'Cocktails created!'
 
